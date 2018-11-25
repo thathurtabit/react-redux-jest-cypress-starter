46c7c25b3259c05374ebbb69903685a8b5c9f55f
Stephen Fairbanks <biggste2000@hotmail.com>
2018-11-25 10:17:22 +0000

    Chore: Add breakpoints to theme.js

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,17 +1,28 @@
 const theme = {
   colors: {
-    brand: "#61dafb"
+    brand: '#61dafb',
   },
   fonts: {
-    baseSize: "16px"
+    baseSize: '16px',
   },
   links: {
-    active: "#fff"
+    active: '#fff',
   },
   transition: {
-    duration: "0.8s",
-    easeOut: "cubic-bezier(0.165, 0.84, 0.44, 1)"
-  }
+    duration: '0.8s',
+    easeOut: 'cubic-bezier(0.165, 0.84, 0.44, 1)',
+  },
+  breakpoints: {
+    sm: '480px',
+    md: '768px',
+    lg: '992px',
+    xl: '1200px',
+    xxl: '1400px',
+  },
+  zIndex: {
+    fixed: 100,
+    overlay: 101,
+  },
 };
 
 export default theme;
